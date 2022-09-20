7cf120b0534aed0f59cfee2893948f034812d8b8
ThiccPan <ihilmiderian@gmail.com>
2022-09-20 15:18:10 +0700

    add event listener when daftar buku jTable is selected

--- src/perpustakaan/FormPeminjaman.java
+++ src/perpustakaan/FormPeminjaman.java
@@ -1,6 +1,8 @@
 package perpustakaan;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,6 +24,26 @@ public class FormPeminjaman extends javax.swing.JFrame{
             public void actionPerformed(ActionEvent e) {
                 String judulBukuPinjam = judulBuku.getText();
                 Perpustakaan.controllerPeminjaman.cariBuku(judulBukuPinjam);
+            }
+        });
+        daftarBuku.setCellSelectionEnabled(true);
+        ListSelectionModel bukuTerpilih = daftarBuku.getSelectionModel();
+        bukuTerpilih.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        bukuTerpilih.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                ArrayList<String> judulBukuAkanDitambah = new ArrayList<>();
+                int[] rows = daftarBuku.getSelectedRows();
+                int[] columns = daftarBuku.getSelectedColumns();
+                for (int row : rows) {
+                    for (int column : columns) {
+                        String judulBukuTerpilih = (String) daftarBuku.getValueAt(row, column);
+                        if (judulBukuTerpilih != null) {
+                            judulBukuAkanDitambah.add(judulBukuTerpilih);
+                        }
+                    }
+                }
+                System.out.println("Judul buku yang dipilih" + judulBukuAkanDitambah);
             }
         });
     }
